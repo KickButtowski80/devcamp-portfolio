6f8cb4f662c18ef8384cdb3b9b848fa46f3765be
Jordan Hudgens <jordan@devcamp.com>
2017-02-14 04:00:44 +0000

    Implemented edit and update functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,5 @@
 class PortfoliosController < ApplicationController
-   # before_action :set_portfolio, only: [:show, :edit, :update, :destroy]
+    #before_action :set_portfolio, only: [:show, :edit, :update, :destroy]
     def index
        @portfolio_items = Portfolio.all
     end
@@ -23,17 +23,30 @@ class PortfoliosController < ApplicationController
         end
     end
     
+    def edit
+      @portfolio_item = Portfolio.find(params[:id])
+    end
     
+    def update
+      @portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
     
     private
     # Use callbacks to share common setup or constraints between actions.
     #def set_portfolio
-    #  @blog = Portfolio.find(params[:id])
+    #  @portfolio_item = Portfolio.find(params[:id])
     #end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def portfolio_params
-      params.require(:portfolio).permit(:title, :subtitle, :body)
+      def portfolio_params
+        params.require(:portfolio).permit(:title, :subtitle, :body)
+      end
     end
-end
 
