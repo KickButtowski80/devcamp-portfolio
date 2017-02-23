53bf35844c3f6c0b0fe96e515aa47b91f64a9d70
Jordan Hudgens <jordan@devcamp.com>
2017-02-23 02:10:23 +0000

    Revert "Implemented image defaults for portfolio items"
    This reverts commit 06d7b1d1a73b2697fce647cce2f8aabe0372a572.

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,22 +6,7 @@ class Portfolio < ApplicationRecord
       where subtitle: "Angular"
   end
   
-  scope :ruby_on_rails_portfolio_items, ->{where subtitle: 'Ruby on Rails'  }
-  
-  after_initialize :set_defaults
-  
-  def set_defaults
-    self.main_image ||= "http://placehold.it/600x400"
-    self.thumb_image ||= "http://placehold.it/350x200"
-  end
+  scope :ruby_on_rails_portfolio_items, ->{
+      where subtitle: 'Ruby on Rails'
+  }
 end
- 
- 
-=begin
-
-meaning of ||=
-
-if self.main_image == nil
-  self.main_image = "http://placehold.it/600x400"
-end
-=end
