a19e0997fb2d00cfb0862bcf1189aad94b291373
Jordan Hudgens <jordan@devcamp.com>
2017-04-15 23:33:54 +0000

    Implemented authorization engine for portfolio items and blogs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
   def login_helper style = ''
+    # is_a? to check type of the class
     if current_user.is_a?(GuestUser)
       (link_to "Register", new_user_registration_path, class: style) +
       "<br> ".html_safe +
