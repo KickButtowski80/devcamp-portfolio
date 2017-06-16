fc8d6b182a31cb0e81874bbc016b44268d5d7a98
Jordan Hudgens <jordan@devcamp.com>
2017-06-16 01:58:13 +0000

    Implemented custom scope for recent blogs and hide draft posts form non admins

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -12,8 +12,12 @@ class BlogsController < ApplicationController
     #binding.pry
     # special_blogs which is scope resides in blog.rb model
     #@blogs = Blog.special_blogs 
+    if logged_in? :site_admin
+      @blogs = Blog.recent.page(params[:page]).per(5)
+    else
+      @blogs = Blog.published.recent.page(params[:page]).per(5)
+    end
     
-    @blogs = Blog.page(params[:page]).per(5)
     @page_title = "My Portfolio Blog"
      
   end
